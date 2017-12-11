8c3e38e602287cb0ef44bfb07bd5a5195be8a71a
gudrun.vene <gudrun.vene@khk.ee>
2017-12-11 12:29:40 +0200

    defineeritud ja väljastatud muutuja

---
+++ mina.php
@@ -0,0 +1,24 @@
+<?php
+
+//üherealine kommentaar
+/*
+*mitme
+*realine
+*kommentaar
+*/
+/*
+ * defineeri oma nimega muutuja,
+ * mille väärtuseks on sinu eesnimi
+ * ja väljasta antud muutuja väärtus
+ * ekraanil koos lisatekstiga
+ * näiteks:
+ * minu nimi on eesnimi
+ */
+$gudrun ="Gudrun";
+//echo
+echo 'echo:Minu nimi on'.$gudrun.'<br />';
+//print funktsiooniga
+print 'print:Minu nimi on'.$gudrun.'<br />';
+//NB! täislause
+print ("print:Minu nimi on$gudrun<br />");
+?>
