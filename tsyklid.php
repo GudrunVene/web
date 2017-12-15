c80c37409f6176daaceecad0482c04587d9269f2
gudrun.vene <gudrun.vene@khk.ee>
2017-12-15 10:47:55 +0200

    väljastamine värviga

--- tsyklid.php
+++ tsyklid.php
@@ -5,6 +5,17 @@
  * Date: 15.12.2017
  * Time: 10:30
  */
+//täienda antud lahendust nii, et paaris arvud oleks
+//punast värvi ja paaritud arvud
+//sinist värvi
+$varv ='';
 for ($arv = 1; $arv <= 10; $arv++){
-    echo $arv.'<br/>';
+    if($arv % 2 == 0){
+        $arv = 'red';
+    }
+    else {
+        $arv = 'blue';
+    }
+    echo '<p style="color:'.$varv.';">'.'</p>';
 }
+
