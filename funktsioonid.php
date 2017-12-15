bbca5682da8f8cafff7626283b9493cf983bf70a
gudrun.vene <gudrun.vene@khk.ee>
2017-12-15 11:56:16 +0200

    värvid loodud

--- funktsioonid.php
+++ funktsioonid.php
@@ -12,6 +12,11 @@
  * tabel, mille pesad on täidetud
  * juhuarvudega vahemikus 10-99
  */
+/*
+ * loo funktsioon genereeriVarv mis genereerib ja tagastab põhiprogrammile
+ * genereeritud värvi kujul #xxxxxxx
+ * vihje: kasuta tsüklis loodud koodi ja teisenda see funktsiooniks
+ */
 function valjastaTabel($ridadeArv, $veergudeArv){
     echo '<table border="1">';
     for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++){
@@ -27,3 +32,14 @@ function valjastaTabel($ridadeArv, $veergudeArv){
     echo '</table>';
 }
 valjastaTabel(5,7);
+function genereeriVarv(){
+    $varv ='';
+    for ($arv = 1; $arv <= 10; $arv++){
+        if($arv % 2 == 0){
+            $arv = 'red';
+        }
+        else {
+            $arv = 'blue';
+        }
+        return $varv;
+}
