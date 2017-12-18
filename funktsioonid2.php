b4a9ab031e369b17583c8b1ae22b673c449abccb
gudrun.vene <gudrun.vene@khk.ee>
2017-12-18 15:21:04 +0200

    mitu korda esineb kindel arv numbris ülesanne lahendatud

--- funktsioonid2.php
+++ funktsioonid2.php
@@ -34,3 +34,31 @@ for ($kord = 1; $kord <= 5; $kord++){
     $number = rand(0, 10000);
     echo 'Numbri '.$number.' arvude summa on '.arvuSumma($number).'<br/>';
 }
+/*
+ * 2. koosta funktsioon nimega otsiNumber,
+ * mis võtab parameetrina suvalise arvu ja
+ * kindlaks määratud arvu ja arvutab mitu korda määratud
+ * arv esineb suvalises numbris, näiteks arv 5
+ * esineb numbris 442158755745 neli korda.
+ * Lahendamiseks ei tohi kasutada sõnetöötluse võimalusi ja massiivitöötluse
+ * võimalusi.
+ *
+ * Funktsioon peab väljastama tulemuse koos seletava tekstiga
+ */
+function otsiNumber($suvalineArv, $kindelArv){
+    echo 'Arv '.$kindelArv.' esineb arvus '.$suvalineArv;
+    $mituKorda = 0;
+    while($suvalineArv != 0) {
+        $arv = $suvalineArv % 10;
+        if($arv == $kindelArv){
+            $mituKorda++;
+        }
+        $suvalineArv = $suvalineArv / 10;
+        settype($suvalineArv, 'integer');
+    }
+    echo ' '.$mituKorda.' korda<br/>';
+}
+otsiNumber(546456,5);
+otsiNumber(6564545245,6);
+otsiNumber(564684512564, 4);
+otsiNumber(461654545456546, 1);
