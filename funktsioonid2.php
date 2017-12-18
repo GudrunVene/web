fe524143f20a7387da18168b3429f7cfefa6eb6b
gudrun.vene <gudrun.vene@khk.ee>
2017-12-18 14:17:39 +0200

    funktsioonid 2 esimese ülesande kirjeldus

---
+++ funktsioonid2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gudrun.vene
+ * Date: 18.12.2017
+ * Time: 14:12
+ */
+/*
+ * 1. koosta funktsioon nimega arvuSumma, mis võtab
+ * parameetrina suvalise numbri ja arvutab numbri
+ * arvude summat - juhul, kui on argumendina
+ * funktsioonile antud number 123, siis programm
+ * peab leidma 1+2+3 summa, ehk 6.
+ * Lahendamiseks ei tohi kasutada sõnetöötluse võimalusi.
+ * Funktsioon peab tagastama leitud summa põhiprogrammile.
+ * Põhiprogrammis kasutada väljatrükis tagastatud väärtust koos
+ * selgitava tekstiga. 
+ */
